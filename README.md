74968a2514c260764366a18fc2228a912941f682
yongxu <xuyhanghang@gmail.com>
2015-11-14 17:54:31 -0700

    add log in and register module

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Dealer
 ======
+An Android app used for family to create shoping list with gcm and data synchronization
+
---
+++ app/src/main/java/yong/dealer/login/library/UserFunctions.java
@@ -0,0 +1,110 @@
+package yong.dealer.login.library;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+import android.content.Context;
+
+
+public class UserFunctions {
+
+    private JSONParser jsonParser;
+	private static final String DOMAIN = "http://ec2-52-2-67-4.compute-1.amazonaws.com/jobscheduleapi/api.php";
+
+    private static String loginURL = DOMAIN;
+    private static String registerURL = DOMAIN;
+    private static String forpassURL = DOMAIN;
+    private static String chgpassURL = DOMAIN;
+
+
+    private static String login_tag = "login";
+    private static String register_tag = "register";
+    private static String forpass_tag = "forpass";
+    private static String chgpass_tag = "chgpass";
+
+
+    // constructor
+    public UserFunctions(){
+        jsonParser = new JSONParser();
+    }
+
+    /**
+     * Function to Login
+     **/
+
+    public JSONObject loginUser(String email, String password){
+        // Building Parameters
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("tag", login_tag));
+        params.add(new BasicNameValuePair("email", email));
+        params.add(new BasicNameValuePair("password", password));
+        JSONObject json = jsonParser.getJSONFromUrl(loginURL, params);
+        return json;
+    }
+
+    /**
+     * Function to change password
+     **/
+
+    public JSONObject chgPass(String newpas, String email){
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("tag", chgpass_tag));
+
+        params.add(new BasicNameValuePair("newpas", newpas));
+        params.add(new BasicNameValuePair("email", email));
+        JSONObject json = jsonParser.getJSONFromUrl(chgpassURL, params);
+        return json;
+    }
+
+
+
+
+
+    /**
+     * Function to reset the password
+     **/
+
+    public JSONObject forPass(String forgotpassword){
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("tag", forpass_tag));
+        params.add(new BasicNameValuePair("forgotpassword", forgotpassword));
+        JSONObject json = jsonParser.getJSONFromUrl(forpassURL, params);
+        return json;
+    }
+
+
+
+
+
+
+     /**
+      * Function to  Register
+      **/
+    public JSONObject registerUser(String fname, String lname, String email, String uname, String password){
+        // Building Parameters
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("tag", register_tag));
+        params.add(new BasicNameValuePair("fname", fname));
+        params.add(new BasicNameValuePair("lname", lname));
+        params.add(new BasicNameValuePair("email", email));
+        params.add(new BasicNameValuePair("uname", uname));
+        params.add(new BasicNameValuePair("password", password));
+        JSONObject json = jsonParser.getJSONFromUrl(registerURL,params);
+        return json;
+    }
+
+
+    /**
+     * Function to logout user
+     * Resets the temporary data stored in SQLite Database
+     * */
+    public boolean logoutUser(Context context){
+        DatabaseHandler db = new DatabaseHandler(context);
+        db.resetTables();
+        return true;
+    }
+
+}
+
